3f72c5e85d60745f228982a2d17a8b69dbc7b933
Tarcísio Luiz <tarcisio.pereira@triersistemas.com.br>
2024-10-09 21:30:34 -0300

    finalizado a construção dos requests de filial

--- estagio_front_end/src/app/services/http.service.ts
+++ estagio_front_end/src/app/services/http.service.ts
@@ -1,10 +1,30 @@
+import { Filial } from './../models/filial';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  // buscarPorQueryParam(id: Number){
+  //   return this.http.get(`comments`, {params : {postId: id.toString()}}).pipe(take(1));
+  // }
+  constructor(private http: HttpClient) { }
 
-  constructor(private httpClient: HttpClient) { }
+  postFilial(filial: Filial){
+    this.http.post('filiais/post', filial).pipe(take(1));
+  }
+  getFilialbyId(id: number){
+    this.http.get(`filiais/get/${id}`).pipe(take(1));
+  }
+  getFilialFiltered(page = 0 , size = 10, nome = "", cnpj = ""){
+    this.http.get('filiais/getAllFilter', {params: {page: page, size: size, nome: nome, cnpj: cnpj}})
+  }
+  putFilial(filial: Filial){
+    this.http.put(`filiais/update/${filial.id}`, filial);
+  }
+  deleteFilial(id: number){
+    this.http.delete(`filiais/delete/${id}`);
+  }
 }
