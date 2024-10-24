85f7cc1cf804f3bdc2d4431f028da6247092b4c2
MatheusTrier <matheus.amaral@triersistemas.com.br>
2024-10-24 10:28:27 -0300

    finalizado

--- estagio_front_end/src/app/main/filial/filial-cadastro/filial-cadastro.component.ts
+++ estagio_front_end/src/app/main/filial/filial-cadastro/filial-cadastro.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
-import { debounce } from 'lodash';
-import { MessageService } from 'primeng/api';
+import { NotFoundComponent } from './../../not-found/not-found.component';
+import { SituacaoCadastro } from './../../../models/app-enums';
+import { Endereco } from './../../../models/filial';
+import { Component, OnInit } from '@angular/core';
+import { Filial } from '../../../models/filial';
 import { SituacaoContrato } from '../../../models/app-enums';
+import { HttpService } from '../../../services/http/http.service';
 import {
   Cep,
   cepToEndereco,
   Cnpj,
-  cnpjEnderecoDisabler,
   cnpjToEndereco,
   cnpjToFilial,
+  cpnjEnderecoDisabler,
   validateCnpj,
-  validateCnpjField,
 } from '../../../models/externalapi';
-import { Filial } from '../../../models/filial';
-import { HttpService } from '../../../services/http/http.service';
-import { MessageHandleService } from '../../../services/message-handle.service';
-import { Endereco } from './../../../models/filial';
+import { MessageService } from 'primeng/api';
+import { debounce } from 'lodash';
 
 @Component({
   selector: 'app-filial-cadastro',
@@ -24,6 +24,130 @@ import { Endereco } from './../../../models/filial';
   providers: [MessageService],
 })
 export class FilialCadastroComponent {
+telefone(): boolean {
+  if(this.cadastroFilial.telefone.replace(/[_]/g, '').length < 15)
+  return true;
+  return false;
+}
+  loading = false;
+  load() {
+    this.loading = true;
+    console.log(this.cadastroFilial.telefone.replace(/[_]/g, ''));
+    let filial = this.cadastroFilial;
+    filial.telefone = this.cadastroFilial.telefone.replace(/[_]/g, '');
+    if (this.endereco.cep !== '') {
+      filial.endereco = this.endereco;
+    }
+    console.log(filial);
+    this.http.postFilial(filial).subscribe({
+      next: (retorno) => {
+        console.log(retorno);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successo',
+          detail: 'Cadastrado com Sucesso',
+        });
+      },
+      error: (erro) => {
+        let error = erro.error;
+        if (typeof error === 'string') {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: error,
+          });
+        } else {
+          let erros = Object.values(error);
+          for (let erro of erros) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: String(erro),
+            });
+          }
+        }
+      },
+    });
+    this.loading = false;
+  }
+  cnpj = debounce(() => {
+    const cnpj = this.cadastroFilial.cnpj.replace(/[_./-]/g, '');
+    console.log(cnpj);
+    if (validateCnpj(cnpj)) {
+      this.http.buscaCNPJ(cnpj).subscribe({
+        next: (obj: Cnpj) => {
+          console.log(obj);
+          if (obj.error) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: obj.error,
+            });
+          }
+
+          this.cadastroFilial = cnpjToFilial(obj);
+          if (obj.CEP !== '') {
+            this.cep(obj);
+          }
+        },
+        error: (error) => {
+          console.log(error);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: error.message,
+            });
+
+        }
+      });
+    }else{
+      const cnpjElement = document.getElementById('cnpj');
+      if(cnpjElement){
+        if (cnpj.length > 0) {
+          cnpjElement.classList.add('ng-invalid');
+          cnpjElement.classList.add('ng-dirty');
+        }else{
+          cnpjElement.classList.remove('ng-invalid');
+          cnpjElement.classList.remove('ng-dirty');
+        }
+      }
+
+    }
+  }, 1000);
+  cadastroSituacao(event: any) {
+    this.cadastroFilial.situacaoContrato = event.value;
+  }
+  constructor(
+    private http: HttpService,
+    private messageService: MessageService
+  ) {}
+  cep = debounce((cnpj?: Cnpj) => {
+    const cep = this.endereco.cep.replace(/[_-]/g, '');
+    console.log(cep);
+    if (cnpj) {
+      this.endereco = cnpjToEndereco(cnpj);
+      this.http.viaCep(cep).subscribe({
+        next: (obj: Cep) => {
+          console.log(obj);
+          cepToEndereco(obj, this.endereco);
+        },
+      });
+      cpnjEnderecoDisabler(cnpj, this.endereco);
+    } else if (cep.length == 8) {
+      this.http.viaCep(cep).subscribe({
+        next: (obj: Cep) => {
+          console.log(obj);
+          cepToEndereco(obj, this.endereco);
+        },
+      });
+    }
+  }, 300);
+  situacaoContrato = Object.keys(SituacaoContrato)
+    .filter((key) => isNaN(Number(key)))
+    .map((status, index) => ({
+      label: status.replace(/_/g, ' '),
+      value: index,
+    }));
   itemSelecionado: any;
   cadastroFilial: Filial = {
     nomeFantasia: '',
@@ -55,138 +179,5 @@ export class FilialCadastroComponent {
       estado: false,
     },
   };
-
-  situacaoContrato = Object.keys(SituacaoContrato)
-    .filter((key) => isNaN(Number(key)))
-    .map((status, index) => ({
-      label: status.replace(/_/g, ' '),
-      value: index,
-    }));
-
-  constructor(
-    private http: HttpService,
-    private messageHandler: MessageHandleService
-  ) {}
-
-  loading = false;
-
-  load() {
-    this.loading = true;
-
-    this.cadastroFilial.telefone = this.cadastroFilial.telefone.replace(
-      /[_]/g,
-      ''
-    );
-
-    if (this.endereco.cep !== '') {
-      this.cadastroFilial.endereco = this.endereco;
-    } else {
-      this.cadastroFilial.endereco = undefined;
-    }
-
-    this.http.postFilial(this.cadastroFilial).subscribe({
-      next: (retorno) => {
-        this.messageHandler.showSuccessMessage(retorno);
-      },
-      error: (erro) => {
-        this.messageHandler.showErrorMessage(erro);
-      },
-    });
-    this.loading = false;
-  }
-
-  cnpj = debounce(() => {
-    const cnpj = this.cadastroFilial.cnpj.replace(/[_./-]/g, '');
-    console.log(cnpj);
-
-    if (cnpj.length == 0) {
-      this.enableFieldsCnpj();
-    }
-
-    if (validateCnpj(cnpj)) {
-      this.http.buscaCNPJ(cnpj).subscribe({
-        next: (obj: Cnpj) => {
-          console.log(obj);
-          if (obj.error) {
-            this.messageHandler.showErrorMessage(obj.error);
-          } else {
-            this.cadastroFilial = cnpjToFilial(obj);
-            if (obj.CEP !== '') {
-              this.cep(obj);
-            }
-          }
-        },
-        error: (error) => {
-          console.log(error);
-        },
-      });
-    } else {
-      validateCnpjField(cnpj);
-    }
-  }, 1000);
-
-  cadastroSituacao(event: any) {
-    this.cadastroFilial.situacaoContrato = event.value;
-  }
-
-  cep = debounce((cnpj?: Cnpj) => {
-    const cep = this.endereco.cep.replace(/[_-]/g, '');
-    console.log(cep.length);
-
-    if (cep.length == 0) {
-      this.enableFieldsAndCleanCep();
-    }
-
-    if (cnpj) {
-      this.endereco = cnpjToEndereco(cnpj);
-      this.http.viaCep(cep).subscribe({
-        next: (obj: Cep) => {
-          console.log(obj);
-          cepToEndereco(obj, this.endereco);
-        },
-        error: (error) => {
-          this.messageHandler.showErrorMessage(error);
-        },
-      });
-      cnpjEnderecoDisabler(cnpj, this.endereco);
-    } else if (cep.length == 8) {
-      this.buscarCep(cep);
-    }
-  }, 300);
-
-  private buscarCep(cep: string) {
-    this.http.viaCep(cep).subscribe({
-      next: (obj: Cep) => {
-        console.log(obj);
-        cepToEndereco(obj, this.endereco);
-      },
-      error: (error) => this.messageHandler.showErrorMessage(error),
-    });
-  }
-
-  isFoneValid(): boolean {
-    return this.cadastroFilial.telefone.replace(/[_]/g, '').length < 15;
-  }
-
-  enableFieldsAndCleanCep() {
-    this.endereco.disabled.bairro = false;
-    this.endereco.disabled.complemento = false;
-    this.endereco.disabled.estado = false;
-    this.endereco.disabled.localidade = false;
-    this.endereco.disabled.logradouro = false;
-    this.endereco.disabled.numero = false;
-
-    this.endereco.bairro = '';
-    this.endereco.complemento = '';
-    this.endereco.estado = '';
-    this.endereco.localidade = '';
-    this.endereco.logradouro = '';
-    this.endereco.numero = undefined;
-  }
-
-  enableFieldsCnpj() {
-    this.cadastroFilial.disabled.email = false;
-    this.cadastroFilial.disabled.nomeFantasia = false;
-    this.cadastroFilial.disabled.razaoSocial = false;
-  }
 }
+
