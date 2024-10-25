31d277a182cb44d2e327ff9b0e295b73fe8c9db4
MatheusTrier <matheus.amaral@triersistemas.com.br>
2024-10-25 08:11:06 -0300

    comitando para troca de pc

--- estagio_front_end/cypress.config.ts
+++ estagio_front_end/cypress.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    watchForFileChanges: false,
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'http://localhost:4200/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const cucumber = require('cypress-cucumber-preprocessor').default
+      on('file:preprocessor', cucumber())
     },
   },
 });
